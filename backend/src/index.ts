4b6c7432977acd78aa459112d049541fe10748db
w.szutryk <w.szutryk@optimumpareto.com>
2023-09-17 20:39:10 +0200

    fix import missmatch at main server file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "apollo-server";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import models from "./models";
 import resolvers from "./graphql/resolvers";
-import typeDefs from "./graphql/typeDefs";
+import typeDefs from "./graphql/types";
 import { $server } from "../config";
 
 const schema = makeExecutableSchema({
